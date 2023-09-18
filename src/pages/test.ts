1cfef1f4cd2368f039a10a29ce53c2fc7f45a9a5
Dominic <Domnick080@outlook.com>
2023-09-18 16:56:52 +0100

    push test --2

--- src/pages/test.ts
+++ src/pages/test.ts
@@ -17,7 +17,7 @@ const handleRegister = async (data) => {
       data
     );
     console.log(res.data);
-    navigate("/verify-email");
+    navigate("/verify-email?auth=true");
     // {
     //   "status": "success",
     //   "data": {
@@ -35,6 +35,52 @@ handleRegister(signupDetails);
 
 
 // 1st page: Tell User to check their mail
-
 // 2nd page:  will display spinner on load
 //  then if token is successful it redirects to the login page
+
+// https://fichat.vercel.app/verify?token=12345&email=test@gmail.com
+
+export default function VerifyFromEmailPage () {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const verifyToken = async (token, email) => {
+    const data = {
+      token: token,
+      // email: email
+    }
+
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_BASE_API_URL}/auth/user/form-verify`,
+        data
+      );
+      console.log(res.data);
+      //   "status": "success",
+      //   "data": {
+      //     "message": "kindly check your email to complete your registration"
+      //   }
+      // }
+      navigate("/dashboard");
+    } catch (error: unknown) {
+      console.log(String(error?.response?.data?.message));
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useLayoutEffect(() => {
+    const token = searchParams.get("token");
+    const email = searchParams.get("email");
+
+    if(!token && !email) {
+      navigate('/login')
+    }  else {
+      verifyToken(token,email)
+    }
+  }, [])
+
+  return (
+
+  )
+}
