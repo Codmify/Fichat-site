4ed014d49a34403f0fd4c573ad970a33c97ee625
Gt <95524505+best2025j@users.noreply.github.com>
2023-09-18 16:13:14 +0100

    Feat/design (#12)
    * profile nav updated
    
    * svg resized
    
    * fixed
    
    * navlink added
    
    * media added
    
    ---------
    
    Co-authored-by: best2025j <audugodtime95@gmai.com>

--- src/pages/components/profile/Profile.tsx
+++ src/pages/components/profile/Profile.tsx
@@ -1,9 +1,37 @@
-
+import DropDownY from "./Dropdown/DropDownY";
+import ImageProfile from "./assets/gt1.jpeg";
+import ProfileImage from "./imageProfile/ProfileImage";
+import ProfileText from "./imageProfile/ProfileText";
 
 const Profile = () => {
   return (
-    <section className="p-6 text-[#8f9198] text-lg font-[700]">Profile Page</section>
-  )
-}
+    <section>
+      <div className="relative">
+        <img src={ImageProfile} alt="no image" className="h-44 w-full" />
 
-export default Profile
+        <div className="p-4 absolute top-0 left-0 right-0 ">
+          <div className="text-white flex items-center justify-between">
+            <h1 className="text-lg font-[700]">My Profile</h1>
+
+            <div className="">
+              <DropDownY />
+            </div>
+          </div>
+        </div>
+
+        <div className="-mt-12">
+          <ProfileImage
+            name="Gt"
+            title="Front end Developer"
+            imageUrl="/gt1.jpeg"
+          />
+        </div>
+        <div className="">
+          <ProfileText />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
