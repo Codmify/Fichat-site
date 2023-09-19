ce61ce7d62bfcbbc8d0767a6c1fb2336caee7704
best2025j <audugodtime95@gmai.com>
2023-09-19 11:15:50 +0100

    changes

--- src/pages/components/profile/Profile.tsx
+++ src/pages/components/profile/Profile.tsx
@@ -1,5 +1,4 @@
-
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 
 const Profile = () => {
@@ -8,7 +7,49 @@ const Profile = () => {
     <section className="bg-[#262626] min-h-screen basis-full lg:basis-[29%] p-4 text-[#8f9198] text-lg font-[700] md:ml-[84px] lg:ml-0 lg:relative md:left-[90px]">Profile Page</section>
     <Outlet />
     </>
-  )
-}
+  );
+};
 
 export default Profile;
+
+// import { Outlet } from "react-router-dom";
+// import DropDownY from "./Dropdown/DropDownY";
+// import ImageProfile from "./assets/gt1.jpeg";
+// import ProfileImage from "./imageProfile/ProfileImage";
+// import ProfileText from "./imageProfile/ProfileText";
+
+// const Profile = () => {
+//   return (
+//     <>
+//       <section>
+//         <div className="relative">
+//           <img src={ImageProfile} alt="no image" className="h-44 w-full" />
+
+//           <div className="p-4 absolute top-0 left-0 right-0 ">
+//             <div className="text-white flex items-center justify-between">
+//               <h1 className="text-lg font-[700]">My Profile</h1>
+
+//               <div className="">
+//                 <DropDownY />
+//               </div>
+//             </div>
+//           </div>
+
+//           <div className="-mt-12">
+//             <ProfileImage
+//               name="Gt"
+//               title="Front end Developer"
+//               imageUrl="/gt1.jpeg"
+//             />
+//           </div>
+
+//           <ProfileText />
+//         </div>
+//       </section>
+
+//       <Outlet />
+//     </>
+//   );
+// };
+
+// export default Profile;
